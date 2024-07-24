761a58a06a6afddc1cf0802fcdb358689498041c
unknown <aricschulz3@gmail.com>
2024-07-23 23:23:35 -0700

    non-finished

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
  
-mongoose.connect("Your MongoDB URI link", {
+mongoose.connect("mongodb://localhost:27017/Stock Market", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
